19af4195ccbca547559a20e4d98c305ff34c81a1
kikeai <lg5967133@gmail.com>
2024-12-26 19:20:51 -0400

    added react-webcam to face route

--- src/app/face/page.tsx
+++ src/app/face/page.tsx
@@ -3,12 +3,19 @@
 import * as faceapi from 'face-api.js';
 import './face.css';
 import React, { useRef, useEffect, useState } from 'react';
+import Webcam from 'react-webcam';
 
 import CameraIcon from '../components/svg/CameraIcon';
+import CloseIcon from '../components/svg/CloseIcon';
+import SwitchCameraIcon from '../components/svg/SwitchCameraIcon';
 
 export default function FacePage() {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const webcamRef: React.LegacyRef<Webcam> = useRef(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const [devices, setDevices] = useState<MediaDeviceInfo[] | null>([]);
+  const [currentDeviceId, setCurrentDeviceId] = useState<ConstrainDOMString | undefined>(undefined);
   const [image, setImage] = useState<string | null>(null);
 
   useEffect(() => {
@@ -34,7 +41,7 @@ export default function FacePage() {
     await faceapi.nets.ssdMobilenetv1.loadFromUri('/models'); // Load SsdMobilenetv1 model
   };
 
-  const handleCapture = async () => {
+  /* const handleCapture = async () => {
     if (videoRef.current && canvasRef.current) {
       const displaySize = { width: videoRef.current.videoWidth, height: videoRef.current.videoHeight };
       canvasRef.current.width = displaySize.width;
@@ -53,6 +60,13 @@ export default function FacePage() {
           .withFaceDescriptors();
         console.log('detections', detections);
       }
+    }
+  }; */
+
+  const handleCap = () => {
+    const imageSrc = webcamRef.current?.getScreenshot();
+    if (imageSrc) {
+      setImage(imageSrc);
     }
   };
 
@@ -77,26 +91,67 @@ export default function FacePage() {
     }
   };
 
+  const handleSwitchCamera = () => {
+    if (devices === null) return;
+    if (devices.length > 1) {
+      const currentIndex = devices.findIndex((device) => device.deviceId === currentDeviceId);
+      const nextIndex = (currentIndex + 1) % devices.length;
+      setCurrentDeviceId(devices[nextIndex].deviceId); // Cambia al siguiente dispositivo
+    }
+  };
+
   useEffect(() => {
     if (image) {
       compareFaces();
     }
   }, [image]);
 
+  useEffect(() => {
+    // Obtén los dispositivos disponibles
+    const getDevices = async () => {
+      try {
+        const devices = await navigator.mediaDevices.enumerateDevices();
+        const videoDevices = devices.filter((device) => device.kind === 'videoinput');
+        setDevices(videoDevices);
+        if (videoDevices.length > 0) {
+          setCurrentDeviceId(videoDevices[0].deviceId); // Por defecto, selecciona la primera cámara
+        }
+      } catch (error) {
+        console.error('Error al enumerar dispositivos:', error);
+      }
+    };
+
+    getDevices();
+  }, []);
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center bg-black">
-      {!image && <video className="h-screen w-full" ref={videoRef} autoPlay muted />}
-      {image && (
-        <div className="flex aspect-square h-auto w-full items-center justify-center sm:h-full sm:w-auto">
-          <img src={image} alt="user-image" className="h-auto w-full object-cover sm:h-full sm:w-auto" />
+      {!image && (
+        <Webcam
+          className="h-full w-full"
+          ref={webcamRef}
+          videoConstraints={{
+            deviceId: currentDeviceId,
+          }}
+        />
+      )}
+      {!image && (
+        <div className="absolute bottom-6 left-1/2 flex -translate-x-1/2 transform items-center gap-4">
+          <button className="rounded-full border-2 border-gray-300 bg-white p-6" onClick={handleCap}>
+            <CameraIcon size={24} color="black" />
+          </button>
+          {devices && devices?.length > 1 && (
+            <button className="rounded-full border-2 border-gray-300 bg-white p-4" onClick={handleSwitchCamera}>
+              <SwitchCameraIcon size={24} color="black" />
+            </button>
+          )}
         </div>
       )}
-      <button
-        className="absolute bottom-6 left-1/2 -translate-x-1/2 transform rounded-full border-2 border-gray-300 bg-white p-6"
-        onClick={handleCapture}
-      >
-        <CameraIcon size={24} color="black" />
-      </button>
+      {image && (
+        <button className="absolute left-4 top-4" onClick={() => setImage(null)}>
+          <CloseIcon size={24} color="white" />
+        </button>
+      )}
+      {image && <img src={image} alt="user-image" className="h-auto w-full sm:h-full sm:w-auto" />}
       <canvas ref={canvasRef} style={{ display: 'none' }} />
     </div>
   );
