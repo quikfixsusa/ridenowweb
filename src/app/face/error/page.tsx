3ff7fd3147dda7fc0913b73e8af9d9096a387087
Luis Garcia <lg5967133@gmail.com>
2025-01-07 12:17:25 -0400

    added error page

---
+++ src/app/face/error/page.tsx
@@ -0,0 +1,10 @@
+import CloseIcon from '@/app/components/svg/CloseIcon';
+import React from 'react';
+
+export default function SuccessMatch() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <CloseIcon size={72} color="red" />
+    </div>
+  );
+}
