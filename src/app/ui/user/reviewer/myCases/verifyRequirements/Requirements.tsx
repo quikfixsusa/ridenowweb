d355e74af571956e0aba58c4c512f60b9afd7063
Luis Garcia <lg5967133@gmail.com>
2025-01-09 10:17:05 -0400

    added verificatio to vehicle requirements

--- src/app/ui/user/reviewer/myCases/verifyRequirements/Requirements.tsx
+++ src/app/ui/user/reviewer/myCases/verifyRequirements/Requirements.tsx
@@ -8,7 +8,7 @@ export default function RequirementsSection({ user }: { user: User }) {
     <div className="flex w-full flex-col gap-4 px-8 py-6">
       {user.vehicle && <CardCar data={user.vehicle} />}
       {user.vehicleRequirements.map((requirement) => (
-        <CardRequirement key={requirement.title} data={requirement} id={user.id} />
+        <CardRequirement key={requirement.title} data={requirement} id={user.id} vehicle={true} />
       ))}
       {user.requirements.map((requirement) => (
         <CardRequirement key={requirement.title} data={requirement} id={user.id} />
