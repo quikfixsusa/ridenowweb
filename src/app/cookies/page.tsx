749dcc48bcf6aff4b83f5b338fb8449ecde47889
Luis Garcia <lg5967133@gmail.com>
2025-02-03 19:22:39 -0400

    added cookies policy

---
+++ src/app/cookies/page.tsx
@@ -0,0 +1,256 @@
+import React from 'react';
+
+const Cookies = () => {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className="w-full bg-white px-8 py-8 md:px-40">
+        <h1 className="mb-8 text-4xl font-bold md:text-[4rem]">RideNow Cookies Policy</h1>
+        <div className="mb-8 h-1 w-full bg-black" />
+        <div className="text-xl text-black">
+          <p className="mb-8">RideNow is owned and operated by Ride Now Taxis Inc.</p>
+          <p className="mb-8">
+            This cookie policy explains how and why cookies and other similar technologies may be stored on and accessed
+            from your device when you use or visit:
+          </p>
+          <ul className="mb-8 list-inside list-disc">
+            <li>
+              <a href="https://www.ridenowtaxis.com" className="text-blue-500">
+                https://www.ridenowtaxis.com
+              </a>
+            </li>
+          </ul>
+          <p className="mb-8">
+            In this cookie policy, the words “website” refer to the RideNow website, “we”, “us”, “our”, and “RideNow”
+            refers to RideNow, and “you” and “user” refers to you, the user and visitor of the RideNow website.
+          </p>
+          <p className="mb-8">
+            This cookie policy should be read together with our privacy policy and our terms and conditions.
+          </p>
+          <p className="mb-8">
+            By using the website, you accept the use of cookies by RideNow, in the terms contained in this policy.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">1. GENERAL INFORMATION</h2>
+          <p className="mb-8">
+            The information collected through cookies will be under responsibility and in charge of:
+          </p>
+          <ul className="mb-8 list-inside list-disc">
+            <li>
+              <strong>Ride Now Taxis Inc.</strong>
+            </li>
+            <li>support@ridenowtaxis.com</li>
+          </ul>
+          <h2 className="mb-8 text-4xl font-semibold">2. WHAT ARE COOKIES?</h2>
+          <p className="mb-8">
+            Cookies are small text files that websites store on the user&apos;s device (computer, smartphone, tablet)
+            when browsing the internet. These cookies contain information that allows websites to remember user
+            preferences, improve the browsing experience and, in some cases, track online activity for analytical or
+            advertising purposes. There are several types of cookies, such as session cookies, which are deleted when
+            the browser is closed, and persistent cookies, which remain on the device for a defined period. Cookies are
+            essential to personalize the user experience and improve the functionality of the website.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">3. CONSENT</h2>
+          <p className="mb-8">
+            Along with this document, when you visit our website for the first time, you will be shown a notice or
+            banner on the home page of the website, informing you about the use of cookies by our website, where you can
+            accept or reject the use of cookies by the website. By clicking ‘Accept’ in this notice, you are giving your
+            explicit consent to our use of cookies on your device. These cookies are used to enhance your browsing
+            experience, personalize content, analyze site traffic and enable specific functionality related to our
+            online operations.
+          </p>
+          <p className="mb-8">
+            Your consent allows us to provide you with a more personalized and efficient user experience. If you decide
+            to withdraw your consent at any time, you can do so by adjusting your browser&apos;s cookie settings.
+            However, please note that refusing to use cookies or disabling certain cookies may reduce the functionality
+            of the site and negatively impact your overall experience.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">4. LEGAL BASIS AND LEGITIMATE INTERESTS</h2>
+          <p className="mb-8">
+            On our website, we use cookies based on our legitimate interests to provide the best possible user
+            experience and to continuously enhance our site. These interests include optimizing our online services,
+            personalizing content and advertising, and ensuring the security and efficiency of our website.
+          </p>
+          <p className="mb-8">
+            Cookies enable us to gain insights into how users interact with our website, allowing us to tailor and
+            improve our services and functionalities to better meet their needs and preferences.
+          </p>
+          <p className="mb-8">
+            Although we believe that our legitimate interests in using cookies do not outweigh your fundamental rights
+            and freedoms, you still have the choice to refuse or manage the use of cookies through your browser settings
+            or by using the tools available on our website.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">5. WITHDRAWAL AND OBJECTION (OPT-OUT)</h2>
+          <p className="mb-8">
+            By accessing our website, you agree to the use of cookies that enhance the browsing experience and allow the
+            personalization of content and advertisements. However, we understand and respect your right to privacy. If
+            you wish to opt-out or exercise your choice to not participate (Opt-Out), you can set your browser to refuse
+            all cookies or to indicate when a cookie is being sent.
+          </p>
+          <p className="mb-8">
+            Please note that some features of the site may not function properly without the use of cookies. In
+            addition, we provide links or tools on our site that allow you to specifically decline the use of certain
+            cookies.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">6. STORAGE DURATION OF COOKIES</h2>
+          <p className="mb-8">
+            The cookies used on our website have different retention periods depending on their purpose. Necessary
+            cookies, which are essential for the basic operation of the site, are stored only for the time necessary to
+            provide the services requested, such as secure access to restricted areas or user authentication. These
+            cookies cannot be disabled, as without them the website would not function properly.
+          </p>
+          <p className="mb-8">
+            Functionality cookies, which allow us to remember your preferences, such as language or personalized site
+            settings, may have a variable duration. Some of these cookies may be session cookies, meaning that they are
+            deleted when you close your browser, while others may be stored for longer periods, up to several years, to
+            provide a smooth and consistent user experience on repeat visits.
+          </p>
+          <p className="mb-8">
+            Analytical cookies allow us to track activity on the website to improve its performance and the user
+            experience. These cookies are kept for a maximum of 24 months before being automatically deleted. You can
+            manage or delete these cookies at any time through your browser settings. However, by disabling or deleting
+            these cookies, some functionality of the site may be limited, which could affect your browsing experience.
+          </p>
+          <p className="mb-8">
+            In addition, there are cookies that can be classified in the ‘other’ category, which include those that do
+            not fit directly into the above categories but are used for other specific purposes, such as improving
+            security or analyzing the more detailed use of the website. These cookies have different durations and, like
+            analytics cookies, can be managed or deleted at the user&apos;s discretion through browser settings.
+          </p>
+          <p className="mb-8">
+            Please note that deleting or rejecting cookies may adversely affect the functionality of the site and may
+            limit some features, impacting the overall experience when interacting with our website.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">7. COOKIE SETTINGS</h2>
+          <p className="mb-8">
+            If you do not want cookies to be dropped on your device, you can adjust the setting of your Internet browser
+            to reject the setting of all or some cookies and to alert you when a cookie is placed on your device. For
+            further information about how to do so, please refer to your browser ‘help’, ‘tool’, or ‘edit’ section.
+            Please note that if you use your browser settings to block all cookies, including strictly necessary
+            cookies, you may not be able to access or use all or parts of the functionalities of RideNow.
+          </p>
+          <p className="mb-8">
+            If you want to remove previously-stored cookies, you can manually delete the cookies at any time. However,
+            this will not prevent RideNow from placing further cookies on your device unless and until you adjust your
+            Internet browser setting as described above.
+          </p>
+          <p className="mb-8">
+            We provide the links for the management and blocking of cookies depending on the browser you use:
+          </p>
+          <ul className="mb-8 list-inside list-disc">
+            <li>
+              <strong>Microsoft Edge: </strong>
+              <a
+                href="https://support.microsoft.com/en-us/office/delete-cookies-in-microsoft-edge-63947406-40ac-c3b8-57b9-2a946a29ae09?ui=en-us&rs=en-us&ad=us"
+                className="text-blue-500"
+              >
+                https://support.microsoft.com/en-us/office/delete-cookies-in-microsoft-edge-63947406-40ac-c3b8-57b9-2a946a29ae09?ui=en-us&rs=en-us&ad=us
+              </a>
+            </li>
+            <li>
+              <strong>Firefox: </strong>
+              <a
+                href="https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox"
+                className="text-blue-500"
+              >
+                https://support.mozilla.org/en-US/kb/clear-cookies-and-site-data-firefox
+              </a>
+            </li>
+            <li>
+              <strong>Chrome: </strong>
+              <a href="https://support.google.com/chrome/answer/95647?hl=en" className="text-blue-500">
+                https://support.google.com/chrome/answer/95647?hl=en
+              </a>
+            </li>
+            <li>
+              <strong>Safari: </strong>
+              <a
+                href="https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac"
+                className="text-blue-500"
+              >
+                https://support.apple.com/guide/safari/manage-cookies-and-website-data-sfri11471/mac
+              </a>
+            </li>
+            <li>
+              <strong>Opera: </strong>
+              <a href="https://help.opera.com/en/latest/web-preferences/" className="text-blue-500">
+                https://help.opera.com/en/latest/web-preferences/
+              </a>
+            </li>
+          </ul>
+          <p className="mb-8">
+            In cases where you access the website through an iOS or Android mobile device, please follow the
+            instructions below to delete or block cookies on your device:
+          </p>
+          <ul className="mb-8 list-inside list-disc">
+            <li>
+              <strong>Android: </strong>
+              <a href="https://support.google.com/answer/32050" className="text-blue-500">
+                https://support.google.com/answer/32050
+              </a>
+            </li>
+            <li>
+              <strong>iOS: </strong>
+              <a href="https://support.apple.com/en-us/HT201265" className="text-blue-500">
+                https://support.apple.com/en-us/HT201265
+              </a>
+            </li>
+          </ul>
+          <h2 className="mb-8 text-4xl font-semibold">8. FIRST-PARTY COOKIES</h2>
+          <p className="mb-8">
+            We use cookies to enhance the performance of our website and personalize your online experience. Cookies
+            help us to collect information on how people use our website and which pages they visit. They enable us to
+            monitor the number of visitors and to analyze website usage patterns and trends. We collect this information
+            anonymously, so it does not identify anyone as an individual and no personal information is stored in our
+            cookies. We always use cookie data in a responsible way.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">9. THIRD-PARTY COOKIES</h2>
+          <p className="mb-8">
+            Third-party cookies may come from partners or third-party companies that provide functional web services or
+            tools for our website and the optimal functioning and operation of our services. We use third party cookies
+            responsibly and for the sole purpose of providing optimal functioning of the platform and services. You may
+            opt out of these cookies by following the cookie removal information contained in this document or the
+            technical information of the browser from which you access our website and services.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">10. PURPOSES OF OUR COOKIES</h2>
+          <p className="mb-8 text-2xl">Our cookies are used for the following purposes:</p>
+          <p className="mb-8">
+            Users who provide information through our platform, as data subjects and data owners, have the right to
+            access, rectify, download or delete their information, as well as to restrict and object to certain
+            processing of their information. While some of these rights apply generally, others apply only in certain
+            limited circumstances. We describe these rights below:
+          </p>
+          <p className="mb-8">
+            <strong>Necessary: </strong> Necessary cookies are essential files that enable basic functions on the
+            website, such as navigation and security. They do not collect personal information.
+          </p>
+          <p className="mb-8">
+            <strong>Functional: </strong> We use functional cookies to enhance your online experience by remembering
+            user preferences and settings and maintaining consistency across the website, without tracking activities.
+          </p>
+          <p className="mb-8">
+            <strong>Analytics: </strong> These cookies help us understand how visitors use our website, by collecting
+            information showing which are the most visited pages and other data regarding an analytical flow. We use
+            this information to create a better experience for all users in our website.
+          </p>
+          <p className="mb-8">
+            <strong>Other:</strong> Cookies in the ‘other’ category include those that do not specifically fit into the
+            necessary, functional or analytical categories. These cookies can fulfil various functions, such as
+            improving the security of the site or providing advanced analysis of the use of the website.
+          </p>
+          <h2 className="mb-8 text-4xl font-semibold">11. CONTACT US</h2>
+          <p className="mb-8">
+            If you have questions or concerns about this cookie policy and the handling and security of your data,
+            please contact us using the contact information below:
+          </p>
+          <ul className="mb-8 list-inside list-disc">
+            <li>
+              <strong>Ride Now Taxis Inc.</strong>
+            </li>
+            <li>support@ridenowtaxis.com</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Cookies;
